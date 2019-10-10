02271dac6222ab5edd608658065395490e6dbfdf
kimj99 <jimankim@pugetsound.edu>
2019-10-09 17:26:50 -0700

    put into constructor

--- Graph.java
+++ Graph.java
@@ -1,4 +1,7 @@
 public class Graph<V> implements GraphIfc<V> {
-    Map<V, List<V>> graphObject = new HashMap<V, List<V>>();
+    protected Map<V, List<V>> graphObject = new HashMap<V, List<V>>();
 
+    public Graph(V vertex) {
+        this.vertex = vertex;
+    }
 }
